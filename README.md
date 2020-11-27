2f57a6e1352333f15801959055c742ec55e4d0ab
Duraan Ali <duraan522@gmail.com>
2020-11-27 04:33:31 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Introduction To JavaScript
+
+The module is the afternoon assignment or task that students work through independently. This expands on the live lecture completed earlier with the instructor.
+
+## JavaScript Foundations
+
+## Objectives
+
+- use let, const, var and demonstrate their differences.
+- understand and be able to use arrays.
+- write a basic for loop / while loop.
+- write control flow using if/else statements.
+- use function declarations, expressions, and arrow
+functions and describe their differences
+  
+## Introduction
+
+Today you'll worth through 5 JavaScript Tasks to practice today's objectives and get familiar and comfortable with the foundations of JavaScript. 
+
+Assignments are outlined in the `index.js` file, please read the instructions carefully for each task and complete it. Note that you may have to use your googling skills to research and look things up if you do not have all the information you need to complete the task.
+
+
+## Instructions
+
+### Task 1: Set up Project
+
+Using VSCode and Command Line:
+
+
+1. Fork the repo
+2. Add your GL as a collaborator.
+3. Clone your forked version of the repo
+4. cd into your repo and create a branch with your first and last name
+4. Read this "README" file and the `index.js` file carefully to do the assignment
+5. Complete your work making regular commits, when you finish the assignment, commit and create a pull request
+
+### Task 2: MVP
+
+Find the `index.js` file and complete the tasks as they're written in there. 
+
+As you work on your code you should make use of `console.log` to check your progress and debug.
+
+### Task 3: Stretch Goals
+
+After you have completed the requirements, try any of the following challenges. As always, note that these may require additional research beyond what you learned in this module.
+
+- [ ] See tasks labelled stretch. Please ensure you've completed MVP before you attempt the stretch goals. 
+
+
+## Resources
+
+🧮 [Polya's 4 Step Approach to Problem Solving](http://web.mnstate.edu/peil/M110/Worksheet/PolyaProblemSolve.pdf)
---
+++ index.js
@@ -0,0 +1,177 @@
+/*🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️ Task 1: Warm-up!🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️*/
+
+/*
+Task 1a - Voting Age
+
+Do the following:   
+   1. Make a variable called votingAge and give it a value
+   2. Return true if age is 18 or higher
+
+   HINT: no function required
+*/
+
+
+
+/*
+Task 1b - Values
+
+Do the following:   
+   1. Declare two variables and assign them values
+   2. Use a conditional to change the value of the 1st variable based on the value assigned to the 2nd variable
+   3. Return the new value of the 1st variable
+
+   HINT: no function required
+*/
+
+
+
+/*
+Task 1c - Convert Strings to Numbers
+
+Do the following:   
+   1. Declare a string type variable with the value of "1999"
+   2. Convert the string value of "1999" to a integer value of 1999
+   3. Return the result
+
+   HINT: look up the Number method
+*/
+
+
+
+/*
+Task 1d - Multiply
+ 
+Do the following:   
+   1. Invoke the multiply function below and pass it two numbers
+   2. Receive the numbers in the parameters: a and b
+   3. Multiply a and b and return the answer
+*/
+
+function multiply(/*add your code here*/){
+    /*add your code here*/
+  }
+
+
+
+/*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
+
+//Age in Cat years
+/*
+Do the following:
+   1. Invoke the catYears function below and pass an age value to it
+   2. Use the received value to calculate the age in cat years (1 human year is equal to 7 cat years)
+   3. Return the newly calculated age
+*/
+
+function catYears(/*add your code here*/){
+    /*add your code here*/
+}
+
+
+/*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
+
+//Grocery Shopping List
+/*
+Do the following:
+   1. Create an array named "shoppingList" with these items "Onion", "Banana", "Bread", "Milk", "Eggs".
+   2. Using the function below "removeItem", remove items as you check them off from your shopping list.
+   3. Every time you invoke the function, it should remove one item.
+   4. Return this statement "Shopping list is empty" when you remove all of the items.
+*/
+
+function removeItem(/*add your code here*/){
+    /*add your code here*/
+}
+
+
+/*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
+
+// Rock, Paper, Scissors - Let's play against the computer!
+/*
+Create a global variable that randomly generates the computer's choice
+Use Math.random to determine the computers choice (Math.random gives a random number between 0 and 1)
+
+HINT: While you can complete this with only conditionals based on strings, it may help to equate choice to a number when using Math.random()
+
+Use the game function below to do the following:
+  1. Receive 2 parameters the user's choice and the computer's choice
+  2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match the strings below exactly.
+   - win should return "you win!"
+   - lose should return "you lose!"
+   - tie should return "it's a tie"
+  
+  RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
+  
+  HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
+*/
+
+function game(user, computer){
+    /*add your code here*/
+}
+  
+
+
+/*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
+
+// Let's Sing 99 Bottles of Soda on the Wall!
+/*
+Using the annoyingSong function below do the following:
+  1. Receive a starting number and start the count down from the number received 
+  2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
+      "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
+*/
+
+function annoyingSong(/*add your code here*/){
+        /*add your code here*/
+  }
+
+
+/*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
+
+//Grade Calculator
+/*
+Using the grade function below do the following: 
+  1. Receive a score out of 100 
+  2. Return the corresponding letter grade following this grade scale:
+
+   90-100 should return 'you got an A' 
+   80-89 should return 'you got a B'
+   70-79 should return 'you got a C'
+   60-69 should return 'you got a D'
+   below should return 'you got an F'
+*/
+  
+function grade(num){
+    if(num < 100 && num >= 90){
+      return 'you got an A';
+    }else if(num <90 && num >= 80){
+      return 'you got a B';
+    }else if(num < 80 && num >= 70){
+      return 'you got a C';
+    }else if(num < 70 && num >= 60){
+      return 'you got a D';
+    }else if(num < 60){
+      return 'you got an F';
+    }
+  }
+  
+  console.log('grade function', grade(85));
+  
+  
+
+/*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
+
+//Vowel Counter - How many vowels are there?
+/*
+Using the vowelCounter function below do the following:
+  1. Receive a string as a parameter
+  2. Count and return the number of vowels within that string.  It should handle both capitalized and uncapitalized vowels.
+
+  HINT - you may need to study tomorrow's content on arrays 
+  HINT - try looking up the .includes() method
+*/
+
+
+function vowelCounter(/*add your code here*/) {
+    /*add your code here*/
+}
